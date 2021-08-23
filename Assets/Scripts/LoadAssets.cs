07991841db8e15bf996901d4ba2e42764eb1b0c8
weijiewang001 <65530009+weijiewang001@users.noreply.github.com>
2021-08-23 22:35:34 +1000

    Revert "60 percent complete"
    This reverts commit 3b34bebf04dc7b11c725bfa7157b6c1bc5270d7e.

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    public GameObject blueObj;
 
     // Start is called before the first frame update
     void Start()

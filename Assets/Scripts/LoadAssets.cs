d86ba804beae6b0367d81b54b924fcd243dce70e
weijiewang001 <65530009+weijiewang001@users.noreply.github.com>
2021-08-23 23:27:05 +1000

    add instantiate property

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
+    [SerializeField] private GameObject blueObj;
+
+
     public GameObject redObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2.0f, 0.0f, 0.0f), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2.0f, 0.0f, 0.0f), Quaternion.identity);
+
     }
 
     // Update is called once per frame

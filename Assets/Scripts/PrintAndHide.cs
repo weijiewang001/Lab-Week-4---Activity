2f910afc37dcc1fe400f8a0f0098fb445026e734
weijiewang001 <65530009+weijiewang001@users.noreply.github.com>
2021-08-25 14:34:29 +1000

    add tag

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
-    int i = 3;
+    [SerializeField] int i = 3;
+    public Renderer rend;
+
     // Start is called before the first frame update
     void Start()
     {
